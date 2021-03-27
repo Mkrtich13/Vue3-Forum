986e7fba45aa395de1b2c2f966d969ec2635b21a
Mkrtich13 <hovanisianm@gmail.com>
2021-03-27 22:19:22 +0100

    update ThreadShow with PostEditor component

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -3,19 +3,22 @@
     <h1>{{ thread.title }}</h1>
 
     <post-list :posts="threadPosts" />
+    <post-editor @save-post="onAddPost" />
   </div>
 </template>
 
 <script>
-  import { computed } from 'vue'
+  import { computed, reactive } from 'vue'
   import PostList from '@/components/PostList'
+  import PostEditor from '@/components/PostEditor'
 
   import data from '@/data.json'
 
   export default {
     name: 'ThreadShow',
     components: {
-      PostList
+      PostList,
+      PostEditor
     },
     props: {
       id: {
@@ -24,17 +27,32 @@
       }
     },
     setup(props) {
+      const posts = reactive(data.posts)
+      const threads = reactive(data.threads)
+
       const thread = computed(() => {
-        return data.threads.find(thread => thread.id === props.id)
+        return threads.find(thread => thread.id === props.id)
       })
 
       const threadPosts = computed(() => {
-        return data.posts.filter(post => post.threadId === props.id)
+        return posts.filter(post => post.threadId === props.id)
       })
+
+      const onAddPost = eventData => {
+        const post = {
+          ...eventData.post,
+          threadId: props.id
+        }
+
+        posts.push(post)
+        thread.value.posts.push(post.id)
+      }
 
       return {
         thread,
-        threadPosts
+        threadPosts,
+        posts,
+        onAddPost
       }
     }
   }
