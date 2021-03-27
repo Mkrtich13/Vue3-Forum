43cbb998abbe687a02b1e0e0badf4083ff38ab5b
Mkrtich13 <hovanisianm@gmail.com>
2021-03-28 00:09:12 +0100

    add auto register globaly App components

--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import { createApp } from 'vue'
 
 import router from '@/router/index'
 import App from '@/App.vue'
-import AppDate from '@/components/AppDate'
 
 const app = createApp(App)
 
 app.use(router)
 
-app.component('AppDate', AppDate)
+const requireComponent = require.context('./', true, /App[A-Z]\w+\.(vue|js)$/)
+requireComponent.keys().forEach(function(fileName) {
+  let baseComponentConfig = requireComponent(fileName)
+  baseComponentConfig = baseComponentConfig.default || baseComponentConfig
+  const baseComponentName =
+    baseComponentConfig.name ||
+    fileName.replace(/^.+\//, '').replace(/\.\w+$/, '')
+
+  app.component(baseComponentName, baseComponentConfig)
+})
+
 app.mount('#app')
